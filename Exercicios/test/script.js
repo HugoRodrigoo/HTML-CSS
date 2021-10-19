f5d14ba189626a8be43cda04f8c5fbafd6bc0a31
HugoRodrigoo <90285975+HugoRodrigoo@users.noreply.github.com>
2021-10-19 11:57:49 -0300

    Initial commit

---
+++ Exercicios/test/script.js
@@ -0,0 +1,7 @@
+var animes = ["jujutsu,Naruto,Dragon boll,kimetsu"]
+var busca = document.getElementById('busca')
+var olha = document.getElementById('')
+function anime() {
+    if(busca == 1)
+    console.log(`${animes[0]}`)
+}
